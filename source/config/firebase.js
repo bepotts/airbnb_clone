1e77f53b5c522cd79fb7509d513a4fecba52aa2e
matttm <matttmaloney@gmail.com>
2022-04-29 08:37:53 -0400

    resetting fb config

--- source/config/firebase.js
+++ source/config/firebase.js
@@ -10,13 +10,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyClxJkjbdcO1EBuQgbtELOenQmBxElNF6k",
-    authDomain: "airbnb-clone-20402.firebaseapp.com",
-    projectId: "airbnb-clone-20402",
-    storageBucket: "airbnb-clone-20402.appspot.com",
-    messagingSenderId: "702193278016",
-    appId: "1:702193278016:web:6ae45a87692548ff0d13a5",
-    measurementId: "G-BFSVNGXN3H"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 };
 
 // Initialize Firebase
